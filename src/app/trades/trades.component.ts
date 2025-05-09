caf8a0dd7c77924b675644819f36a999213810a6
Óscar García <geoscar1100@gmail.com>
2025-05-10 00:21:26 +0200

    Refactorized and solved CSS problems

--- src/app/trades/trades.component.ts
+++ src/app/trades/trades.component.ts
@@ -32,14 +32,8 @@ export class TradesComponent {
     {
       id: '1',
       weapons: {
-        offered: [
-          this.test[1],
-          this.test[1],
-          this.test[1],
-          this.test[1],
-          this.test[1],
-        ],
-        received: [this.test[0], this.test[1], this.test[1], this.test[1]],
+        offered: [this.test[1], this.test[1], this.test[1]],
+        received: [this.test[0], this.test[0], this.test[0], this.test[0]],
       },
       creationDate: new Date(),
     },
